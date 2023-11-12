5941d265c37ec42fb673596d1d598c9f41385495
Antonín Štoll <antoninstoll@gmail.com>
2023-11-12 14:55:30 +0100

    refactoring, new class for resolver

--- main.cpp
+++ main.cpp
@@ -82,11 +82,6 @@ public:
     }
 };
 
-
-bool udpSend(const char *msg){
-    
-}
-
 struct DNSHeader {
     uint16_t ID;            // DNS Query Identifier
     uint16_t Flags;         // Flags
@@ -96,55 +91,131 @@ struct DNSHeader {
     uint16_t ARCount;       // Number of Additional Resource Records
 };
 
-// setting dns header
-void SetDNSHeader(DNSHeader* header, Configuration config){
-    header->ID = htons(45);
+class Resolver{
+public:
+    DNSHeader header = DNSHeader();
+    uint8_t  query[QUERY_LENGTH]{};
+    Configuration config;
 
-    uint16_t flags = 0;
-    if (config.inverse){
-        flags |= INVERSEBIT;
+    explicit Resolver(Configuration conf){
+        config = conf;
+        SetDNSHeader();
+        SetDNSQuestion();
     }
-    if (config.recursion){
-        flags |= RECURSIONBIT;
-    }
 
-    header->Flags = htons(flags);
-    header->QDCount = htons(1);
-    header->ANCount = htons(0);
-    header-> NSCount = htons(0);
-    header->ARCount = htons(0);
-}
+    // setting dns header
+    void SetDNSHeader(){
+        header.ID = htons(45);
 
-// setting dns question
-void SetDNSQuestion(uint8_t* query, Configuration config){
-    int position = 0;
-    int length = 0;
-    while (config.adress[position]){
-        if (config.adress[position] != '.'){
-            query[position + 1] = config.adress[position];
-            length++;
-        } else {
-            query[position - length] = length;
-            length = 0;
+        uint16_t flags = 0;
+        if (config.inverse){
+            flags |= INVERSEBIT;
+        }
+        if (config.recursion){
+            flags |= RECURSIONBIT;
         }
 
-        position++;
+        header.Flags = htons(flags);
+        header.QDCount = htons(1);
+        header.ANCount = htons(0);
+        header. NSCount = htons(0);
+        header.ARCount = htons(0);
     }
-    query[position - length] = length;
-    position++;
-    query[position] = 0;
-    position++;
 
-    uint16_t queryType = htons(config.aaaa ? 28 : 1); // A or AAAA record type
-    memcpy(&query[position], &queryType, sizeof(queryType));
-    position += sizeof(queryType);
+    // setting dns question
+    void SetDNSQuestion(){
+        int position = EncodeLabel(config.adress, query);
 
-    uint16_t queryClass = htons(1); // IN class
-    memcpy(&query[position], &queryClass, sizeof(queryClass));
-    position += sizeof(queryClass);
+        uint16_t queryType = htons(config.aaaa ? 28 : 1); // A or AAAA record type
+        memcpy(&query[position], &queryType, sizeof(queryType));
+        position += sizeof(queryType);
 
-}
+        uint16_t queryClass = htons(1); // IN class
+        memcpy(&query[position], &queryClass, sizeof(queryClass));
+    }
 
+    void SendQuestion(){
+        // combining header and question
+        uint8_t msg[MSG_LENGTH];
+        memcpy(msg, &header, sizeof(header));
+        memcpy(&msg[sizeof(header)], query, sizeof(query));
+
+        std::cout << "Formatted DNS Request Packet:" << std::endl;
+        for (int i = 0; i < 256; i++) {
+            printf("%02X ", msg[i]);
+            if ((i + 1) % 16 == 0)
+                std::cout << std::endl;
+        }
+        std::cout << "-----------------------" << std::endl;
+
+
+        int sock;                        // socket descriptor
+        int i;
+        struct sockaddr_in server, from; // address structures of the server and the client
+
+        socklen_t len, fromlen;
+
+        memset(&server,0,sizeof(server)); // erase the server structure
+        server.sin_family = AF_INET;
+        server.sin_addr.s_addr = inet_addr(config.server);
+        server.sin_port = htons(53);
+
+        if ((sock = socket(AF_INET , SOCK_DGRAM , 0)) == -1){  //create a client socket
+            // error
+        }
+
+        len = sizeof(server);
+        fromlen = sizeof(from);
+
+        if (connect(sock, (struct sockaddr *)&server, sizeof(server))  == -1){
+            // error
+        }
+
+        i = send(sock,msg,256,0);
+        if (i == -1){
+            // err
+        }
+
+        getsockname(sock,(struct sockaddr *) &from, &len);
+
+        uint8_t answer[512];
+        i = recv(sock,answer, 512,0);
+        printf("ahoj");
+        std::cout << "Formatted DNS Request Packet:" << std::endl;
+        for (int i = 0; i < 256; i++) {
+            printf("%02X ", answer[i]);
+            if ((i + 1) % 16 == 0)
+                std::cout << std::endl;
+        }
+        std::cout << std::endl;
+    }
+
+    static int EncodeLabel(const char *src, uint8_t *dst){
+        int position = 0;
+        int length = 0;
+        while (src[position]){
+            if (src[position] != '.'){
+                dst[position + 1] = src[position];
+                length++;
+            } else {
+                dst[position - length] = length;
+                length = 0;
+            }
+            position++;
+        }
+
+        dst[position - length] = length;
+        position++;
+        dst[position] = 0;
+        position++;
+
+        return position;
+    }
+
+    void DecodeLabel(){
+
+    }
+};
 
 int main(int argc, char* argv[]) {
     // parsing command line arguments
@@ -154,65 +225,8 @@ int main(int argc, char* argv[]) {
         return EXIT_FAILURE;
     }
 
-    // creating DNS question and header
-    uint8_t  query[QUERY_LENGTH];
-    DNSHeader header = DNSHeader();
+    auto resolver = Resolver(config);
+    resolver.SendQuestion();
 
-    SetDNSHeader(&header, config);
-    SetDNSQuestion(query, config);
-
-    // combining header and question
-    uint8_t msg[MSG_LENGTH];
-    memcpy(msg, &header, sizeof(header));
-    memcpy(&msg[sizeof(header)], query, sizeof(query));
-
-    std::cout << "Formatted DNS Request Packet:" << std::endl;
-    for (int i = 0; i < 256; i++) {
-        printf("%02X ", msg[i]);
-        if ((i + 1) % 16 == 0)
-            std::cout << std::endl;
-    }
-    std::cout << "-----------------------" << std::endl;
-
-
-    int sock;                        // socket descriptor
-    int i;
-    struct sockaddr_in server, from; // address structures of the server and the client
-
-    socklen_t len, fromlen;
-
-    memset(&server,0,sizeof(server)); // erase the server structure
-    server.sin_family = AF_INET;
-    server.sin_addr.s_addr = inet_addr(config.server);
-    server.sin_port = htons(53);
-
-    if ((sock = socket(AF_INET , SOCK_DGRAM , 0)) == -1){  //create a client socket
-        // error
-    }
-
-    len = sizeof(server);
-    fromlen = sizeof(from);
-
-    if (connect(sock, (struct sockaddr *)&server, sizeof(server))  == -1){
-        // error
-    }
-
-    i = send(sock,msg,256,0);
-    if (i == -1){
-        // err
-    }
-
-    getsockname(sock,(struct sockaddr *) &from, &len);
-
-    uint8_t answer[512];
-    i = recv(sock,answer, 512,0);
-    printf("ahoj");
-    std::cout << "Formatted DNS Request Packet:" << std::endl;
-    for (int i = 0; i < 256; i++) {
-        printf("%02X ", answer[i]);
-        if ((i + 1) % 16 == 0)
-            std::cout << std::endl;
-    }
-    std::cout << std::endl;
     return 0;
 }
